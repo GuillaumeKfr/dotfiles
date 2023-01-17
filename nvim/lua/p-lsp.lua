8b3d41b7a739cc499215807e321fab59a4bfeb41
Orokh <guillaume.kaiffer@gmail.com>
2023-01-17 18:47:29 +0100

    feat: rework neovim config

---
+++ nvim/lua/p-lsp.lua
@@ -0,0 +1,83 @@
+local lsp = require('lsp-zero')
+
+lsp.preset('recommended')
+
+lsp.ensure_installed({
+    'sumneko_lua'
+})
+
+local cmp = require('cmp')
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),
+})
+
+lsp.setup_nvim_cmp({
+    mapping = cmp_mappings
+})
+
+local on_attach = function(_, bufnr)
+    local opts = { buffer = bufnr, remap = false }
+
+    -- [R]e[n]ame
+    vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+    -- [C]ode [A]ction
+    vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+
+    -- [G]oto [D]efinition
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    -- [G]oto [R]eferences
+    vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("i", "<C-k>", function() vim.lsp.buf.signature_help() end, opts)
+
+    -- [W]orkspace [S]ymbols
+    vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    -- [D]ocument [S]ymbols
+    vim.keymap.set("n", "<leader>ds", function() vim.lsp.buf.document_symbol() end, opts)
+
+    -- Diagnostics
+    vim.keymap.set("n", "<leader>d", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+
+    -- format on save
+    vim.api.nvim_create_autocmd('BufWritePre', {
+        group = vim.api.nvim_create_augroup('LspFormatting', { clear = true }),
+        buffer = bufnr,
+        callback = function()
+            vim.lsp.buf.format()
+        end
+    })
+end
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+local lsp_config = {
+    capabilities = capabilities,
+    on_attach = function(_, bufnr)
+        on_attach(_, bufnr)
+    end
+}
+require('mason-lspconfig').setup_handlers({
+    function(server_name)
+        require('lspconfig')[server_name].setup(lsp_config)
+    end,
+    sumneko_lua = function()
+        require('lspconfig').sumneko_lua.setup(vim.tbl_extend('force', lsp_config, {
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { 'vim' }
+                    }
+                }
+            }
+        }))
+    end
+})
+
+lsp.setup()
---
+++ nvim/lua/set.lua
@@ -0,0 +1,38 @@
+vim.g.mapleader = ' '
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.autoread = true
+vim.bo.autoread = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+vim.opt.updatetime = 250
+
+vim.opt.colorcolumn = "80"
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 300
+        })
+    end
+})
