377189148de4c8e120748240c18ddcd9036397d0
Orokh <guillaume.kaiffer@gmail.com>
2023-01-18 13:57:29 +0100

    feat: rework lualine display

--- nvim/lua/p-lualine.lua
+++ nvim/lua/p-lualine.lua
@@ -1,8 +1,16 @@
 require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    theme = 'auto',
-    component_separators = '|',
-    section_separators = '',
-  },
+    options = {
+        icons_enabled = true,
+        theme = 'auto',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { { 'filename', path = 1 } },
+        lualine_x = {},
+        lualine_y = { '' },
+        lualine_z = { 'filetype' }
+    },
 }
