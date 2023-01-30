3c7236d400c6035d432f19a7e1d15dd1f734fc72
Orokh <guillaume.kaiffer@gmail.com>
2023-01-30 01:41:27 +0100

    feat: use bubbles in lualine

--- nvim/lua/p-lualine.lua
+++ nvim/lua/p-lualine.lua
@@ -2,15 +2,21 @@ require('lualine').setup {
     options = {
         icons_enabled = true,
         theme = 'auto',
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
+        component_separators = '|',
+        section_separators = { left = '', right = '' },
     },
     sections = {
-        lualine_a = { 'mode' },
+        lualine_a = {{
+            'mode',
+            separator = { left = '' },
+        }},
         lualine_b = { 'branch', 'diff', 'diagnostics' },
         lualine_c = { { 'filename', path = 1 } },
         lualine_x = {},
-        lualine_y = { '' },
-        lualine_z = { 'filetype' }
+        lualine_y = {},
+        lualine_z = {{
+            'filetype',
+            separator = { right = '' }
+        }}
     },
 }
