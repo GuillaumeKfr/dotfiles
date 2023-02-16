461a5c2c08643395f41c879543e7be467a7d78d4
Orokh <guillaume.kaiffer@gmail.com>
2023-02-17 00:16:14 +0100

    feat: add comment plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -24,6 +24,13 @@ return require('packer').startup(function(use)
     }
 
     use {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end
+    }
+
+    use {
         'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
