4b5bf707d170aa43866c7e0efef60776e79618f0
Orokh <guillaume.kaiffer@gmail.com>
2023-06-06 02:20:32 +0200

    feat: rework global structure & use stow

--- install_env.sh
+++ install_env.sh
@@ -1,44 +1,92 @@
-#! /bin/sh
+#! /bin/bash
 
+# Env
 DOTFILES_DIR="$HOME"/dotfiles
 ZSH_CUSTOM_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
 ZSH_PLUGINS_DIR="${ZSH_CUSTOM_DIR}/plugins"
 ZSH_THEMES_DIR="${ZSH_CUSTOM_DIR}/themes"
 
-# Install and enable zsh
-sudo apt update
-sudo apt upgrade
-sudo apt install -y zsh
-chsh -s "$(which zsh)"
+CLIP_DEPS=(
+    build-essential 
+    libcairo2-dev 
+    libdbus-glib-1-dev 
+    libfuse2 
+    libgirepository1.0-dev 
+    libsystemd-dev 
+    pkg-config 
+    python3-pip
+    ripgrep 
+    unzip 
+)
 
-# Link zsh config
-ln -sf "$DOTFILES_DIR"/zsh/zshrc "$HOME"/.zshrc
-ln -sf "$DOTFILES_DIR"/zsh/p10k.zsh "$HOME"/.p10k.zsh
+CLI_TOOLS=(
+    jq
+    shellcheck
+    tmux
+    unzip
+    zsh
+)
 
-# Install oh-my-zsh and plugins
+PIP_DEPS=(
+    pip-review
+)
+
+PIP_TOOLS=(
+    black
+    sqlfluff
+)
+
+STOW_DIRS=(
+    nvim
+    zsh
+)
+
+echo "== SYS == Upgrade packages"
+sudo apt update && apt upgrade -y
+
+echo "== CLI == Installing dependencies"
+sudo apt install -y "${CLIP_DEPS[@]}"
+
+echo "== CLI == Installing tools"
+sudo apt install -y "${CLI_TOOLS[@]}"
+
+echo "== PIP == Installing dependencies"
+pip install --upgrade "${PIP_DEPS[@]}"
+
+echo "== PIP == Upgrade packages"
+pip-review --auto
+
+echo "== PIP == Installing tools"
+pip install --upgrade "${PIP_TOOLS[@]}"
+
+echo "== CFG == Stow configuration files"
+pushd "${DOTFILES_DIR}" || exit
+for dir in "${STOW_DIRS[@]}"; do
+    echo "stow $dir"
+    stow -D "${dir}"
+    stow "${dir}"
+done
+popd || exit
+
+echo "== ZSH == Install OMZ"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "== ZSH == Install plugins"
+mkdir -p "${ZSH_PLUGINS_DIR}"
 
 git clone "https://github.com/zsh-users/zsh-autosuggestions.git" "${ZSH_PLUGINS_DIR}"/zsh-autosuggestions
 git clone "https://github.com/zsh-users/zsh-syntax-highlighting.git" "${ZSH_PLUGINS_DIR}"/zsh-syntax-highlighting
 git clone "https://github.com/agkozak/zsh-z" "${ZSH_PLUGINS_DIR}"/zsh-z
 git clone --depth=1 "https://github.com/romkatv/powerlevel10k.git" "${ZSH_THEMES_DIR}"/powerlevel10k
 
-# Link oh-my-zsh config
-ln -sf "$DOTFILES_DIR"/oh-my-zsh/alias.zsh "${ZSH_CUSTOM_DIR}"/alias.zsh
-
-# Install TMUX
-
-sudo apt install -y tmux
-
-git clone "https://github.com/tmux-plugins/tpm" "$HOME"/.tmux/plugins/tpm
-
-# Link tmux config
-ln -sf "$DOTFILES_DIR"/tmux/tmux.conf "$HOME"/.tmux.conf
+echo "== Change shell"
+chsh -s "$(which zsh)"
 
 # Install neovim
 
-curl -LO https://github.com/neovim/neovim/releases/download/v0.8.2/nvim-linux64.deb
-sudo apt install ./nvim-linux64.deb
+echo "== VIM == Install NeoVim"
+curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+chmod u+x nvim.appimage
+sudo mv nvim.appimage /usr/local/bin
 
-ln -sf "$DOTFILES"/nvim "$HOME"/.config/nvim
 
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -126,3 +126,105 @@ export GOPATH=$HOME/go
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 
 export PATH=~/bin:$PATH
+
+
+# ####### #
+# Aliases #
+# ####### #
+#
+# General purpose
+alias v="nvim ."
+alias py="python3"
+alias tf="terraform"
+alias calc="bc -l"
+alias mkdir="mkdir -pv"
+alias rm="rm -I --preserve-root"
+alias update="sudo apt update && sudo apt upgrade"
+alias ping="ping -c 5"
+
+if [[ -v WSL_DISTRO_NAME ]]; then
+    function clip() {
+        cat $1 | clip.exe
+    }
+fi
+
+if [[ -x "$(command -v exa)" ]]; then
+    alias ls="exa --group-directories-first --icons"
+    alias ll="ls --long"
+    alias la="ll --all"
+    alias tree="ll --tree"
+fi
+
+if [[ -x "$(command -v podman)" ]]; then
+    alias docker="podman"
+fi
+
+docker_aliases() {
+    alias jupyter="docker run --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v ${PWD}:/home/jovyan/work docker.io/jupyter/minimal-notebook"
+    alias jupyterStop="docker stop $(docker ps -a -f "ancestor=jupyter/minimal-notebook" -q)"
+
+    alias dk="docker"
+    alias dkl="docker logs"
+    alias dklf="docker logs -f"
+    alias dki="docker images"
+    alias dkc="docker container"
+    alias dkrm="docker rm"
+
+    alias dke="docker exec"
+    dksh() {
+        docker exec -it $1 /bin/sh
+    }
+
+    alias dkps="docker ps --format '{{.ID}} ~ {{.Names}} ~ {{.Status}} ~ {{.Image}}'"
+    alias dktop="docker stats --format 'table {{.Container}}\t{{.Name}}\t{{.CPUPerc}}\t{{.MemPerc}}\t{{.NetIO}}\t{{.BlockIO}}'"
+    dkstats() {
+        if [[ $# -eq 0 ]]; then
+            docker stats --no-stream
+        else
+            docker stats --no-stream | grep $1
+        fi
+    }
+
+    alias dkprune="docker system prune -af"
+}
+# Docker
+if [[ -x "$(command -v docker)" ]]; then
+    docker_aliases
+fi
+
+# Git
+git_aliases() {
+    alias g="git"
+
+    alias ga="git add"
+    alias gaa="git add --all"
+
+    alias gc="git commit -v -m"
+    alias gc!="git commit -v --amend"
+
+    alias gaac="gaa && gc"
+
+    alias gb="git branch --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(contents:subject) %(color:green)(%(committerdate:relative)) [%(authorname)]' --sort=-committerdate"
+    alias gs="git status -sb"
+
+    alias gst="git stash"
+    alias gsta="git stash apply"
+    alias gstp="git stash pop"
+
+    alias gco="git checkout"
+
+    alias gd="git diff"
+
+    alias gl="git pull"
+
+    alias glog="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset'"
+
+    alias gm="git merge"
+
+    alias gp="git push"
+
+    alias grb="git rebase"
+    alias grba="git rebase --abort"
+    alias grbc="git rebase --continue"
+}
+git_aliases
