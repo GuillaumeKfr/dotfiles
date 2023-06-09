7731348b5f903d0b1bfc612e5a3602883b74950a
Orokh <guillaume.kaiffer@gmail.com>
2023-06-09 22:01:58 +0200

    feat: rework logging

--- install_env.sh
+++ install_env.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+SCRIPT_DIR="$( dirname -- "${BASH_SOURCE[0]}" )"
+
+source "${SCRIPT_DIR}/lib/logging.sh"
+
 # Env
 DOTFILES_DIR="$HOME"/dotfiles
 ZSH_CUSTOM_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
@@ -51,41 +55,41 @@ STOW_DIRS=(
     zsh
 )
 
-echo "== SYS == Upgrade packages"
+logging::info "== SYS == Upgrade packages"
 sudo apt -qq update && apt -qq upgrade -y
 
-echo "== SYS == Adding extra repos"
+logging::info "== SYS == Adding extra repos"
 sudo apt-add-repository -qq "${APT_EXTRA_REPOS[@]}"
 
-echo "== CLI == Installing dependencies"
+logging::info "== CLI == Installing dependencies"
 sudo apt -qq install -y "${APT_DEPS[@]}"
 
-echo "== CLI == Installing Homebrew"
+logging::info "== CLI == Installing Homebrew"
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-echo "== CLI == Installing tools"
+logging::info "== CLI == Installing tools"
 brew install -q "${BREW_INSTALLS[@]}"
 
-echo "== PIP == Installing tools"
+logging::info "== PIP == Installing tools"
 pip install --quiet --upgrade "${PIP_INSTALLS[@]}"
 
-echo "== PIP == Upgrade packages"
+logging::info "== PIP == Upgrade packages"
 pip-review --auto --quiet
 
-echo "== CFG == Stow configuration files"
+logging::info "== CFG == Stow configuration files"
 pushd "${DOTFILES_DIR}" || exit
 for dir in "${STOW_DIRS[@]}"; do
-    echo "stow $dir"
+    logging::info "stow $dir"
     stow -D "${dir}"
     stow "${dir}"
 done
 popd || exit
 
-echo "== ZSH == Install OMZ"
+logging::info "== ZSH == Install OMZ"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-echo "== ZSH == Install plugins"
+logging::info "== ZSH == Install plugins"
 mkdir -p "${ZSH_PLUGINS_DIR}"
 
 rm -rf "${ZSH_PLUGINS_DIR}"/zsh-autosuggestions
@@ -97,18 +101,20 @@ git clone --quiet "https://github.com/zsh-users/zsh-syntax-highlighting.git" "${
 git clone --quiet "https://github.com/agkozak/zsh-z" "${ZSH_PLUGINS_DIR}"/zsh-z
 git clone --quiet --depth=1 "https://github.com/romkatv/powerlevel10k.git" "${ZSH_THEMES_DIR}"/powerlevel10k
 
-echo "== Fish == Install Fisher"
+logging::info "== Fish == Install Fisher"
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 
-echo "== Fish == Install plugins"
+logging::info "== Fish == Install plugins"
 fisher install kidonng/zoxide.fish
 
-echo "== SSH == Create private key"
+logging::info "== SSH == Create private key"
 if [[ ! -d ~/.ssh ]]; then
-    echo "== No existing config found. Generating key."
+    logging::info "== No existing config found. Generating key."
+else
+    logging::warn "SSH config already existing. Skipping section."
 fi
 
-echo "== Change shell"
+logging::info "== User == Change shell"
 shell_opts=(zsh fish)
 select user_shell in "${shell_opts[@]}"; do
     chsh -s "$(which "${user_shell}")"
@@ -116,3 +122,4 @@ select user_shell in "${shell_opts[@]}"; do
 done
 chsh -s "$(which fish)"
 
+logging::success "Installation complete."
---
+++ libs/logging.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+
+reset_color=$(tput sgr 0)
+
+logging::info() {
+  printf "%s[*] %s%s\n" "$(tput setaf 4)" "$1" "$reset_color"
+}
+
+logging::success() {
+  printf "%s[✔️] %s%s\n" "$(tput setaf 2)" "$1" "$reset_color"
+}
+
+logging::err() {
+  printf "%s[❌] %s%s\n" "$(tput setaf 1)" "$1" "$reset_color"
+}
+
+logging::warn() {
+  printf "%s[⚠] %s%s\n" "$(tput setaf 3)" "$1" "$reset_color"
+}
