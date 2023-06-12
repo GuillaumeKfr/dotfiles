c721943ddee60a3b67f4ae10c7339d9106e2a389
Orokh <guillaume.kaiffer@gmail.com>
2023-06-13 01:29:20 +0200

    fix: reorder user inputs

--- install_env.sh
+++ install_env.sh
@@ -61,39 +61,40 @@ STOW_DIRS=(
 )
 
 logging::info "== SYS == Upgrade packages"
-sudo apt -qq update && sudo apt -qq upgrade -y
 wait_input
+sudo apt -qq update && sudo apt -qq upgrade -y
 
 logging::info "== SYS == Adding extra repos"
+wait_input
 if [[ ${#APT_EXTRA_REPOS[@]} > 0 ]]; then
     sudo apt-add-repository "${APT_EXTRA_REPOS[@]}"
 else
     logging::warn "No extra repo to add"
 fi
-wait_input
 
 logging::info "== CLI == Installing dependencies"
-sudo apt -qq install -y "${APT_DEPS[@]}"
 wait_input
+sudo apt -qq install -y "${APT_DEPS[@]}"
 
 logging::info "== CLI == Installing Homebrew"
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 wait_input
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 logging::info "== CLI == Installing tools"
-brew install -q "${BREW_INSTALLS[@]}"
 wait_input
+brew install -q "${BREW_INSTALLS[@]}"
 
 logging::info "== PIP == Installing tools"
-pip install --quiet --upgrade "${PIP_INSTALLS[@]}"
 wait_input
+pip install --quiet --upgrade "${PIP_INSTALLS[@]}"
 
 logging::info "== PIP == Upgrade packages"
-~/.local/bin/pip-review --auto --quiet
 wait_input
+~/.local/bin/pip-review --auto --quiet
 
 logging::info "== CFG == Stow configuration files"
+wait_input
 pushd "${DOTFILES_DIR}" || exit
 for dir in "${STOW_DIRS[@]}"; do
     logging::info "stow $dir"
@@ -101,13 +102,13 @@ for dir in "${STOW_DIRS[@]}"; do
     stow "${dir}"
 done
 popd || exit
-wait_input
 
 logging::info "== ZSH == Install OMZ"
-ZSH=~/.oh-my-zsh sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 wait_input
+ZSH=~/.oh-my-zsh sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 logging::info "== ZSH == Install plugins"
+wait_input
 mkdir -p "${ZSH_PLUGINS_DIR}"
 
 rm -rf "${ZSH_PLUGINS_DIR}"/zsh-autosuggestions
@@ -116,29 +117,29 @@ rm -rf "${ZSH_PLUGINS_DIR}"/zsh-z
 git clone --quiet "https://github.com/zsh-users/zsh-autosuggestions.git" "${ZSH_PLUGINS_DIR}"/zsh-autosuggestions
 git clone --quiet "https://github.com/zsh-users/zsh-syntax-highlighting.git" "${ZSH_PLUGINS_DIR}"/zsh-syntax-highlighting
 git clone --quiet "https://github.com/agkozak/zsh-z" "${ZSH_PLUGINS_DIR}"/zsh-z
-wait_input
 
 logging::info "== Fish == Set as valid shell"
-which fish | sudo tee -a /etc/shells > /dev/null
 wait_input
+which fish | sudo tee -a /etc/shells > /dev/null
 
 logging::info "== Fish == Install Fisher"
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 wait_input
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 
 logging::info "== Fish == Install plugins"
-fisher install kidonng/zoxide.fish
 wait_input
+fisher install kidonng/zoxide.fish
 
 logging::info "== SSH == Create private key"
+wait_input
 if [[ ! -d ~/.ssh ]]; then
     logging::info "== No existing config found. Generating key."
 else
     logging::warn "SSH config already existing. Skipping section."
 fi
-wait_input
 
 logging::info "== User == Change shell"
+wait_input
 shell_opts=(zsh fish)
 select user_shell in "${shell_opts[@]}"; do
     chsh -s "$(which "${user_shell}")"
