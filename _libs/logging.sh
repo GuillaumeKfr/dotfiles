43c632f834e1ce10106c19b2bbe1aa3b049a2d8a
Orokh <guillaume.kaiffer@gmail.com>
2023-06-13 00:17:45 +0200

    feat: add icon for info logs

--- _libs/logging.sh
+++ _libs/logging.sh
@@ -4,7 +4,7 @@
 reset_color=$(tput sgr 0)
 
 logging::info() {
-  printf "%s[*] %s%s\n" "$(tput setaf 4)" "$1" "$reset_color"
+  printf "%s[ℹ️] %s%s\n" "$(tput setaf 4)" "$1" "$reset_color"
 }
 
 logging::success() {
