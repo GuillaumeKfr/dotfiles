bc48847441d8f41f25db4bd729faaa4f3db553b8
Orokh <guillaume.kaiffer@gmail.com>
2023-10-15 18:43:14 +0200

    fix: correct telescope setup, with fzf

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -5,9 +5,16 @@ return require("packer").startup(function(use)
 
 	use({
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.1",
-		requires = { { "nvim-lua/plenary.nvim" } },
+        branch = "0.1.x",
+		requires = {
+            { "nvim-lua/plenary.nvim" },
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                run = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
+            }
+        }
 	})
+
     use ({ 'nvim-tree/nvim-web-devicons' })
 	--    use { 'dracula/vim', as = 'dracula' }
 	use({ "catppuccin/nvim", as = "catppuccin" })
