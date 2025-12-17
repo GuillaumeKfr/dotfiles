32f9ceea90487867981491f939012a0bd6e2087a
GuillaumeKfr <guillaume.kaiffer@sanofi.com>
2025-12-17 16:54:58 +0100

    feat(fish): add flag to allow skipping emai/pwd prompt

--- fish/.config/fish/functions/awslog.fish
+++ fish/.config/fish/functions/awslog.fish
@@ -1,8 +1,20 @@
 # awslog: Login to AWS using saml2aws and set AWS_PROFILE.
+# Usage: awslog [-s|--skip-prompt]
 function awslog
+    # Parse arguments passed to the function
+    argparse 's/skip-prompt' -- $argv
+    or return
+
+    # Handle optional s/skip_prompt flag
+    set -l skip_prompt
+    if set -ql _flag_s
+        set skip_prompt --skip-prompt
+    end
+
     # Present a menu of AWS profiles from ~/.aws/credentials
-    set account  $(grep '\[.*\]' ~/.aws/credentials | tr -d '[]' | fzf)
+    set account $(grep '\[.*\]' ~/.aws/credentials | tr -d '[]' | fzf)
+
     # Login using saml2aws and set AWS_PROFILE
-	saml2aws login -a $account -p $account --force $skip_prompt
-	set -gx AWS_PROFILE $account
+    saml2aws login -a $account -p $account --force $skip_prompt
+    set -gx AWS_PROFILE $account
 end
