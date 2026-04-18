eda654c912a73b3c1ca6f41c1286b53b076ce966
GuillaumeKfr <guillaume.kaiffer@sanofi.com>
2026-04-18 20:18:17 +0200

    Squashed 'nvim/.config/nvim/' content from commit aae5c4e
    git-subtree-dir: nvim/.config/nvim
    git-subtree-split: aae5c4e52161d5b20a3184b6d1d32124f0420fe3

---
+++ lua/custom/init.lua
@@ -0,0 +1,28 @@
+local file_exists = function(name)
+  local f = io.open(name, 'r')
+  if f ~= nil then
+    io.close(f)
+    return true
+  else
+    return false
+  end
+end
+
+if file_exists '/etc/wsl.conf' then
+  vim.g.clipboard = {
+    name = 'WSLClip',
+    copy = {
+      ['+'] = 'clip.exe',
+      ['*'] = 'clip.exe',
+    },
+    paste = {
+      ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+  }
+end
+
+-- Copy to system clipboard
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+Y')
---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,24 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    lazy = false,
+    config = function()
+      require('oil').setup {
+        view_options = { show_hidden = true },
+        keymaps = {
+          ['<C-h>'] = false,
+          ['<C-l>'] = false,
+        },
+      }
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = '[-] Open parent directory' })
+    end,
+  },
+}
