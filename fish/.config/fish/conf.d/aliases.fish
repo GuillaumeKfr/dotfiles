74d9b7a45ebdc3a9929ec288ffd9f0d370f5dd25
GuillaumeKfr <guillaume.kaiffer@sanofi.com>
2026-07-06 16:52:33 +0200

    feat(fish): add puffer-fish expansions (!!, !$, !*, ...)
    Vendor puffer-fish (MIT) for command-line expansions. Replaces the
    hand-rolled bind_bang/bind_dollar, which it supersets.

--- fish/.config/fish/conf.d/aliases.fish
+++ fish/.config/fish/conf.d/aliases.fish
@@ -36,27 +36,3 @@ function mkdir -d "Create a directory and set CWD"
         end
     end
 end
-
-function bind_bang
-    switch (commandline -t)[-1]
-        case "!"
-            commandline -t -- $history[1]
-            commandline -f repaint
-        case "*"
-            commandline -i !
-    end
-end
-
-function bind_dollar
-    switch (commandline -t)[-1]
-        case "!"
-            commandline -f backward-delete-char history-token-search-backward
-        case "*"
-            commandline -i '$'
-    end
-end
-
-function fish_user_key_bindings
-    bind -M insert ! bind_bang
-    bind -M insert '$' bind_dollar
-end
